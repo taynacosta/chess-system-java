3a96c768395d597e49a711bfcdb2ea71b5de55bc
taynacosta <taynamoreira1811@gmail.com>
2021-04-25 13:45:16 -0300

    ChessExpetin and position

---
+++ src/chess/ChessExeption.java
@@ -0,0 +1,11 @@
+package chess;
+
+public class ChessExeption extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ChessExeption (String msg) {
+		super (msg);
+	}
+	
+
+}
