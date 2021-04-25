4a8b46e323130d90c38999e93fd426161abeb393
taynacosta <taynamoreira1811@gmail.com>
2021-04-25 10:14:52 -0300

    placing pieces on the board

--- src/chess/ChessMacth.java
+++ src/chess/ChessMacth.java
@@ -1,12 +1,16 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMacth {
 	private Board board;
 	
 	public ChessMacth() {
 		board = new Board(8,8);
+		initialSetup();
 	}
 
 	public ChessPiece [][] getPieces(){
@@ -17,5 +21,11 @@ public class ChessMacth {
 			}
 		}
 		return mat;
+	}
+	
+	private void initialSetup() {//inicia a partida
+		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
+		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+		board.placePiece(new King(board, Color.BLACK), new Position(7, 4));
 	}
 }
